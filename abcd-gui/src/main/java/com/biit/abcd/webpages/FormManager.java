ab9c1e887f8c44e1b87587efa208eb0fbad053b9
Jorge Izquierdo Ciges <jorizci@gmail.com>
2014-05-08 16:19:04 +0200

    WebPageComponent added as a new base class for all webpages. FormManager and Login page adapted to use it.

--- abcd-gui/src/main/java/com/biit/abcd/webpages/FormManager.java
+++ abcd-gui/src/main/java/com/biit/abcd/webpages/FormManager.java
@@ -1,13 +1,24 @@
 package com.biit.abcd.webpages;
 
-import com.biit.abcd.webpages.basic.DWebPage;
+import com.biit.abcs.component.WebPageComponent;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
 
-public class FormManager extends DWebPage {
+public class FormManager extends WebPageComponent {
 	private static final long serialVersionUID = 8306642137791826056L;
 
+	private VerticalLayout rootLayout;
+	
+	public FormManager() {
+		rootLayout = new VerticalLayout();
+		rootLayout.setSizeFull();
+		setCompositionRoot(rootLayout);
+		
+		setSizeFull();
+	}
+	
 	@Override
-	public void init(ViewChangeEvent event) {
+	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
 
 	}
