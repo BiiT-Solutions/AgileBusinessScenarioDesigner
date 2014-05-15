dff83eb402b40079984e918fc1011b0865557529
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-05-15 17:45:30 +0200

    Old Form Manager removed.

--- abcd-gui/src/main/java/com/biit/abcd/webpages/elements/formtable/CollapsibleCellLabel.java
+++
@@ -1,7 +0,0 @@
-package com.biit.abcd.webpages.elements.formtable;
-
-import com.vaadin.ui.HorizontalLayout;
-
-abstract class CollapsibleCellLabel extends HorizontalLayout implements Comparable<CollapsibleCellLabel> {
-	private static final long serialVersionUID = -721622752018794079L;
-}
--- abcd-gui/src/main/java/com/biit/abcd/webpages/elements/formtable/FormsTableColumns.java
+++
@@ -1,5 +0,0 @@
-package com.biit.abcd.webpages.elements.formtable;
-
-enum FormsTableColumns {
-	NAME, VERSION, ACCESS, USED_BY, CREATED_BY, CREATION_DATE, MODIFIED_BY, MODIFICATION_DATE;
-}
