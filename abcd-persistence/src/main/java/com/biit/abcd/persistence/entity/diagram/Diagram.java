1a0e0b429794f37d7233679e8049997ee567ffe8
Alfonso <aperez@biit-solutions.com>
2014-07-15 16:42:59 +0200

    Added editable select diagram table. Now the name of the diagrams can be changed in the table.

--- abcd-persistence/src/main/java/com/biit/abcd/persistence/entity/diagram/Diagram.java
+++ abcd-persistence/src/main/java/com/biit/abcd/persistence/entity/diagram/Diagram.java
@@ -28,13 +28,14 @@ import com.biit.abcd.gson.utils.DiagramSourceSerializer;
 import com.biit.abcd.gson.utils.DiagramTableSerializer;
 import com.biit.abcd.persistence.entity.Form;
 import com.biit.abcd.persistence.entity.StorableObject;
+import com.biit.abcd.persistence.utils.ITableCellEditable;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
 
 @Entity
 @Table(name = "DIAGRAM")
-public class Diagram extends StorableObject {
+public class Diagram extends StorableObject implements ITableCellEditable {
 
 	private String name;
 
