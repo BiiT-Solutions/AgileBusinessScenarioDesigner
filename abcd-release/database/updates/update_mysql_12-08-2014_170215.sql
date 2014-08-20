683f69dd469b02b1978f4405b91c11705447cf34
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-08-20 10:58:06 +0200

    MySQL scripts already imported scripts deleted.

--- abcd-release/database/updates/update_mysql_12-08-2014_170215.sql
+++
@@ -1,3 +0,0 @@
-
-    alter table PARENT_OF_CHILDREN 
-        add column orders_index integer not null;
--- abcd-release/database/updates/update_mysql_19-08-2014_110214.sql
+++
@@ -1,6 +0,0 @@
-
-    alter table parent_of_children 
-        drop constraint UK_3awgy2uyqhop13ni86af4ufgv;
-
-    alter table parent_of_children 
-        add constraint UK_3awgy2uyqhop13ni86af4ufgv  unique (children_ID);
