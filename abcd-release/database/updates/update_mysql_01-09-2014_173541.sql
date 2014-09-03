08ac8b950436ac9d478ecca91ce3754273446dea
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-09-03 12:44:47 +0200

    Mysql invalid scripts removed.

--- abcd-release/database/updates/update_mysql_01-09-2014_173541.sql
+++ abcd-release/database/updates/update_mysql_01-09-2014_173541.sql
@@ -1,9 +1,3 @@
 
     alter table expression_value_timestamp 
         add column systemDate bit not null;
-
-    alter table form_custom_variables 
-        drop constraint UK_2pj0qoh0ntvs9laf9sh42rqap;
-
-    alter table form_custom_variables 
-        add constraint UK_2pj0qoh0ntvs9laf9sh42rqap  unique (form, name, scope);
