54ab3cb4d20bc9260067cc782bb4a67b22c57503
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-09-17 10:18:25 +0200

    ZIP file for downloading.

--- abcd-gui/src/main/java/com/biit/abcd/webpages/elements/formmanager/SaveDroolsRulesAction.java
+++ abcd-gui/src/main/java/com/biit/abcd/webpages/elements/formmanager/SaveDroolsRulesAction.java
@@ -1,6 +1,11 @@
 package com.biit.abcd.webpages.elements.formmanager;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import com.biit.abcd.MessageManager;
 import com.biit.abcd.authentication.UserSessionHandler;
@@ -14,24 +19,39 @@ import com.biit.abcd.webpages.components.SaveAction;
 import com.biit.abcd.webpages.components.SettingsWindow;
 
 public class SaveDroolsRulesAction implements SaveAction {
+	public static final String FILE_CODIFICATION = "UTF-8";
 
 	@Override
 	public String getMimeType() {
-		return "text/plain";
+		return "application/zip";
 	}
 
 	@Override
 	public String getExtension() {
-		return "txt";
+		return "zip";
 	}
 
 	@Override
 	public byte[] getInformationData() {
 		try {
+			List<String> filesToZip = new ArrayList<>();
+			List<String> namesOfFiles = new ArrayList<>();
 			FormToDroolsExporter droolsExporter = new FormToDroolsExporter();
-			//TODO Global Variables not included. 
 			String rules = droolsExporter.getDroolRules(UserSessionHandler.getFormController().getForm(), null);
-			return rules.getBytes();
+			filesToZip.add(rules);
+			namesOfFiles.add("droolRules.drl");
+			String variables =
+					filesToZip.add(variables);
+					namesOfFiles.add("globalVariables.json");
+
+			if (filesToZip != null && filesToZip.size() > 0 && filesToZip.size() == namesOfFiles.size()) {
+				try {
+					return zipFiles(filesToZip, namesOfFiles);
+				} catch (IOException e) {
+					MessageManager.showError("Error generating zip file with the XML files.");
+					e.printStackTrace();
+				}
+			}
 		} catch (ExpressionInvalidException | RuleInvalidException | IOException e) {
 			MessageManager.showError(LanguageCodes.ERROR_DROOLS_INVALID_RULE, e.getMessage());
 			AbcdLogger.errorMessage(SettingsWindow.class.getName(), e);
@@ -44,6 +64,21 @@ public class SaveDroolsRulesAction implements SaveAction {
 			AbcdLogger.errorMessage(SettingsWindow.class.getName(), e);
 		}
 		return null;
+	}
+
+	private byte[] zipFiles(List<String> files, List<String> names) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ZipOutputStream zos = new ZipOutputStream(baos);
+		for (int i = 0; i < files.size(); i++) {
+			String fileToZip = files.get(i);
+			ZipEntry entry = new ZipEntry(names.get(i));
+			entry.setSize(fileToZip.length());
+			zos.putNextEntry(entry);
+			zos.write(fileToZip.getBytes(FILE_CODIFICATION));
+		}
+		zos.closeEntry();
+		zos.close();
+		return baos.toByteArray();
 	}
 
 	@Override
