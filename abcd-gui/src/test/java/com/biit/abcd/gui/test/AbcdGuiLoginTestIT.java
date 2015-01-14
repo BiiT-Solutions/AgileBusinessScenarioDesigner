05c061161c48a1cfcf94f82169d637b67a19ec7b
Alfonso <aperez@biit-solutions.com>
2015-01-14 11:10:02 +0100

    Gui login test working

--- abcd-gui/src/test/java/com/biit/abcd/gui/test/AbcdGuiLoginTestIT.java
+++ abcd-gui/src/test/java/com/biit/abcd/gui/test/AbcdGuiLoginTestIT.java
@@ -2,28 +2,31 @@ package com.biit.abcd.gui.test;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 import com.vaadin.testbench.TestBenchTestCase;
 import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.testbench.elements.PasswordFieldElement;
-import com.vaadin.testbench.elements.TableElement;
 import com.vaadin.testbench.elements.TextFieldElement;
 
 public class AbcdGuiLoginTestIT extends TestBenchTestCase {
 
 	@Before
 	public void setUp() throws Exception {
-		setDriver(new FirefoxDriver());
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("intl.accept_languages", "en_US");
+		setDriver(new FirefoxDriver(profile));
 	}
 
+	@Test
 	public void testAbcdLogin() {
 		// Get the page and log in
-		getDriver().get("http://localhost:8081");
+		getDriver().get("http://localhost:9081");
 		$(TextFieldElement.class).id("userNameLoginForm").setValue("test@liferay.com");
 		$(PasswordFieldElement.class).id("userPassLoginForm").setValue("test");
 		$(ButtonElement.class).caption("Sign In").first().click();
-		$(TableElement.class).first();
 	}
 
 	@After
