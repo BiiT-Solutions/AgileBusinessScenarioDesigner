34a0058202112e19f74d3e9dd2d6ce27e6b7e88c
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-12-10 10:24:48 +0100

    Updated configuration to new Commons.

--- abcd-persistence/src/main/java/com/biit/abcd/configuration/AbcdConfigurationReader.java
+++ abcd-persistence/src/main/java/com/biit/abcd/configuration/AbcdConfigurationReader.java
@@ -6,7 +6,7 @@ import com.biit.abcd.logger.AbcdLogger;
 import com.biit.utils.configuration.ConfigurationReader;
 import com.biit.utils.configuration.PropertiesSourceFile;
 import com.biit.utils.configuration.SystemVariablePropertiesSourceFile;
-import com.biit.utils.configuration.exception.PropertyNotFoundException;
+import com.biit.utils.configuration.exceptions.PropertyNotFoundException;
 
 public class AbcdConfigurationReader extends ConfigurationReader {
 
