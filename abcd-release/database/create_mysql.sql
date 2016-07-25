4450cb86464b7712111bdb2b3a96c4bea2661175
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-07-25 10:00:27 +0200

    updated pom structure

--- abcd-release/database/create_mysql.sql
+++ abcd-release/database/create_mysql.sql
@@ -1554,13 +1554,3 @@
         add constraint FK_mb6o16g0xsjlo3nx5xoc0i0g9 
         foreign key (tree_forms_ID) 
         references tree_forms (ID);
-
-	CREATE TABLE `hibernate_sequence` (
-		`next_val` bigint(20) DEFAULT NULL
-	);
-
-	LOCK TABLES `hibernate_sequence` WRITE;
-	INSERT INTO `hibernate_sequence` VALUES (1);
-	UNLOCK TABLES;
-
-
