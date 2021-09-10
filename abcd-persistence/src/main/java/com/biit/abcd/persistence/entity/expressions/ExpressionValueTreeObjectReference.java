8807e561e4b7aaea865252681639592f268965f5
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-09-10 12:19:12 +0200

    Avoiding a stackoverflow

--- abcd-persistence/src/main/java/com/biit/abcd/persistence/entity/expressions/ExpressionValueTreeObjectReference.java
+++ abcd-persistence/src/main/java/com/biit/abcd/persistence/entity/expressions/ExpressionValueTreeObjectReference.java
@@ -1,6 +1,7 @@
 package com.biit.abcd.persistence.entity.expressions;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -18,95 +19,97 @@ import com.biit.persistence.entity.exceptions.NotValidStorableObjectException;
 @Entity
 @Table(name = "expression_value_tree_object_reference")
 public class ExpressionValueTreeObjectReference extends ExpressionValue<TreeObject> {
-	private static final long serialVersionUID = 3933694492937877414L;
+    private static final long serialVersionUID = 3933694492937877414L;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="reference")
-	private TreeObject reference;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "reference")
+    private TreeObject reference;
 
-	@Enumerated(EnumType.STRING)
-	private QuestionDateUnit unit = null;
+    @Enumerated(EnumType.STRING)
+    private QuestionDateUnit unit = null;
 
-	public ExpressionValueTreeObjectReference() {
-		super();
-	}
+    public ExpressionValueTreeObjectReference() {
+        super();
+    }
 
-	public ExpressionValueTreeObjectReference(TreeObject reference) {
-		super();
-		this.reference = reference;
-	}
+    public ExpressionValueTreeObjectReference(TreeObject reference) {
+        super();
+        this.reference = reference;
+    }
 
-	public ExpressionValueTreeObjectReference(TreeObject reference, QuestionDateUnit unit) {
-		super();
-		this.reference = reference;
-		this.unit = unit;
-	}
+    public ExpressionValueTreeObjectReference(TreeObject reference, QuestionDateUnit unit) {
+        super();
+        this.reference = reference;
+        this.unit = unit;
+    }
 
-	public TreeObject getReference() {
-		return reference;
-	}
+    public TreeObject getReference() {
+        return reference;
+    }
 
-	public void setReference(TreeObject reference) {
-		this.reference = reference;
-	}
+    public void setReference(TreeObject reference) {
+        this.reference = reference;
+    }
 
-	public synchronized QuestionDateUnit getUnit() {
-		return unit;
-	}
+    public synchronized QuestionDateUnit getUnit() {
+        return unit;
+    }
 
-	public synchronized void setUnit(QuestionDateUnit unit) {
-		this.unit = unit;
-	}
+    public synchronized void setUnit(QuestionDateUnit unit) {
+        this.unit = unit;
+    }
 
-	@Override
-	public String getRepresentation(boolean showWhiteCharacter) {
-		if (unit != null) {
-			return reference + " (" + unit.getAbbreviature() + ")";
-		}
-		return "" + reference;
-	}
+    @Override
+    public String getRepresentation(boolean showWhiteCharacter) {
+        if (unit != null) {
+            return reference + " (" + unit.getAbbreviature() + ")";
+        }
+        return "" + reference;
+    }
 
-	@Override
-	protected String getExpression() {
-		return "" + getReference();
-	}
+    @Override
+    protected String getExpression() {
+        return "" + getReference();
+    }
 
-	@Override
-	public TreeObject getValue() {
-		return getReference();
-	}
+    @Override
+    public TreeObject getValue() {
+        return getReference();
+    }
 
-	@Override
-	public void setValue(TreeObject value) {
-		this.reference = value;
-	}
+    @Override
+    public void setValue(TreeObject value) {
+        this.reference = value;
+    }
 
-	@Override
-	public Set<StorableObject> getAllInnerStorableObjects() {
-		Set<StorableObject> innerStorableObjects = new HashSet<>();
-		if (reference != null) {
-			innerStorableObjects.add(reference);
-			innerStorableObjects.addAll(reference.getAllInnerStorableObjects());
-		}
-		return innerStorableObjects;
-	}
+    @Override
+    public Set<StorableObject> getAllInnerStorableObjects() {
+        Set<StorableObject> innerStorableObjects = new HashSet<>();
+        if (reference != null) {
+            innerStorableObjects.add(reference);
+            if (!Objects.equals(reference.getComparationId(), this.getComparationId())) {
+                innerStorableObjects.addAll(reference.getAllInnerStorableObjects());
+            }
+        }
+        return innerStorableObjects;
+    }
 
-	@Override
-	public void copyData(StorableObject object) throws NotValidStorableObjectException {
-		if (object instanceof ExpressionValueTreeObjectReference) {
-			super.copyData(object);
-			ExpressionValueTreeObjectReference expressionValueTreeObjectReference = (ExpressionValueTreeObjectReference) object;
-			setUnit(expressionValueTreeObjectReference.getUnit());
-			// Rule tables can have empty expressions with null inside
-			if (expressionValueTreeObjectReference.getValue() != null) {
-				// Later the reference must be updated with current
-				// TreeObject
-				setValue(expressionValueTreeObjectReference.getValue());
-			}
-		} else {
-			throw new NotValidStorableObjectException("Object '" + object
-					+ "' is not an instance of ExpressionValueTreeObjectReference.");
-		}
-	}
+    @Override
+    public void copyData(StorableObject object) throws NotValidStorableObjectException {
+        if (object instanceof ExpressionValueTreeObjectReference) {
+            super.copyData(object);
+            ExpressionValueTreeObjectReference expressionValueTreeObjectReference = (ExpressionValueTreeObjectReference) object;
+            setUnit(expressionValueTreeObjectReference.getUnit());
+            // Rule tables can have empty expressions with null inside
+            if (expressionValueTreeObjectReference.getValue() != null) {
+                // Later the reference must be updated with current
+                // TreeObject
+                setValue(expressionValueTreeObjectReference.getValue());
+            }
+        } else {
+            throw new NotValidStorableObjectException("Object '" + object
+                    + "' is not an instance of ExpressionValueTreeObjectReference.");
+        }
+    }
 
 }
