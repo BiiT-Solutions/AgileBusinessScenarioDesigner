41162730929e7b06f4181c133d553d7a22965643
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-03-31 12:55:28 +0200

    Updated to new User Manager library

--- abcd-core/src/main/java/com/biit/abcd/core/security/SecurityService.java
+++ abcd-core/src/main/java/com/biit/abcd/core/security/SecurityService.java
@@ -1,199 +1,247 @@
 package com.biit.abcd.core.security;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
+import com.biit.abcd.logger.AbcdLogger;
 import com.biit.abcd.persistence.entity.Form;
 import com.biit.usermanager.entity.IGroup;
 import com.biit.usermanager.entity.IRole;
 import com.biit.usermanager.entity.IUser;
 import com.biit.usermanager.security.IActivity;
+import com.biit.usermanager.security.IActivityManager;
 import com.biit.usermanager.security.IAuthenticationService;
 import com.biit.usermanager.security.IAuthorizationService;
+import com.biit.usermanager.security.exceptions.InvalidCredentialsException;
+import com.biit.usermanager.security.exceptions.OrganizationDoesNotExistException;
 import com.biit.usermanager.security.exceptions.UserDoesNotExistException;
 import com.biit.usermanager.security.exceptions.UserManagementException;
 import com.liferay.portal.log.SecurityLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class SecurityService implements ISecurityService {
 
-	@Autowired
-	private IAuthenticationService<Long, Long> authenticationService;
+    @Autowired
+    private IAuthenticationService<Long, Long> authenticationService;
 
-	@Autowired
-	private IAuthorizationService<Long, Long, Long> authorizationService;
+    @Autowired
+    private IAuthorizationService<Long, Long, Long> authorizationService;
 
-	protected SecurityService() {
-		super();
-	}
+    @Autowired
+    private IActivityManager<Long, Long, Long> activityManager;
 
-	public IGroup<Long> getDefaultGroup(IUser<Long> user) {
-		try {
-			return getAuthenticationService().getDefaultGroup(user);
-		} catch (UserManagementException e) {
-			SecurityLogger.errorMessage(this.getClass().getName(), e);
-		}
-		return null;
-	}
+    protected SecurityService() {
+        super();
+    }
 
-	public Set<IActivity> getActivitiesOfRoles(List<IRole<Long>> roles) {
-		Set<IActivity> activities = new HashSet<>();
-		for (IRole<Long> role : roles) {
-			activities.addAll(getAuthorizationService().getRoleActivities(role));
-		}
-		SecurityLogger.debug(this.getClass().getName(), "Activities for roles '" + roles + "' are '" + activities + "'.");
-		return activities;
-	}
+    public IGroup<Long> getDefaultGroup(IUser<Long> user) {
+        try {
+            return getAuthenticationService().getDefaultGroup(user);
+        } catch (UserManagementException | UserDoesNotExistException | InvalidCredentialsException e) {
+            SecurityLogger.errorMessage(this.getClass().getName(), e);
+        }
+        return null;
+    }
 
-	@Override
-	public boolean isUserAuthorizedInAnyOrganization(IUser<Long> user, IActivity activity)
-			throws UserManagementException {
-		// Check isUserAuthorizedActivity (own permissions)
-		if (getAuthorizationService().isAuthorizedActivity(user, activity)) {
-			SecurityLogger.info(this.getClass().getName(), "User '" + user + "' is authorized for '" + activity + "'.");
-			return true;
-		}
+    public Set<IActivity> getActivitiesOfRoles(List<IRole<Long>> roles) {
+        Set<IActivity> activities = new HashSet<>();
+        for (IRole<Long> role : roles) {
+            try {
+                activities.addAll(getActivityManager().getRoleActivities(role));
+            } catch (InvalidCredentialsException e) {
+                AbcdLogger.errorMessage(this.getClass().getName(), e);
+            }
+        }
+        SecurityLogger.debug(this.getClass().getName(), "Activities for roles '" + roles + "' are '" + activities + "'.");
+        return activities;
+    }
 
-		// Get all organizations of user
-		Set<IGroup<Long>> organizations = getUserOrganizations(user);
-		for (IGroup<Long> organization : organizations) {
-			if (isAuthorizedActivity(user, organization, activity)) {
-				SecurityLogger.info(this.getClass().getName(),
-						"User '" + user + "' has authorization for '" + activity + "' at '" + organization + "'.");
-				return true;
-			}
-		}
-		SecurityLogger.info(this.getClass().getName(), "User '" + user + "' is NOT authorized for '" + activity + "'.");
-		return false;
-	}
+    @Override
+    public boolean isUserAuthorizedInAnyOrganization(IUser<Long> user, IActivity activity)
+            throws UserManagementException {
+        // Check isUserAuthorizedActivity (own permissions)
+        try {
+            if (getActivityManager().isAuthorizedActivity(user, activity)) {
+                SecurityLogger.info(this.getClass().getName(), "User '" + user + "' is authorized for '" + activity + "'.");
+                return true;
+            }
+        } catch (UserDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
 
-	public boolean isAuthorizedActivity(IUser<Long> user, Form form, IActivity activity) {
-		if (form == null || form.getOrganizationId() == null) {
-			return false;
-		}
-		return isAuthorizedActivity(user, form.getOrganizationId(), activity);
-	}
+        // Get all organizations of user
+        Set<IGroup<Long>> organizations = getUserOrganizations(user);
+        for (IGroup<Long> organization : organizations) {
+            if (isAuthorizedActivity(user, organization, activity)) {
+                SecurityLogger.info(this.getClass().getName(),
+                        "User '" + user + "' has authorization for '" + activity + "' at '" + organization + "'.");
+                return true;
+            }
+        }
+        SecurityLogger.info(this.getClass().getName(), "User '" + user + "' is NOT authorized for '" + activity + "'.");
+        return false;
+    }
 
-	@Override
-	public boolean isAuthorizedActivity(IUser<Long> user, Long organizationId, IActivity activity) {
-		if (organizationId == null) {
-			return false;
-		}
-		IGroup<Long> organization = getOrganization(user, organizationId);
-		if (organization == null) {
-			return false;
-		}
-		try {
-			return getAuthorizationService().isAuthorizedActivity(user, organization, activity);
-		} catch (UserManagementException e) {
-			SecurityLogger.errorMessage(this.getClass().getName(), e);
-			// For security
-			return false;
-		}
-	}
+    public boolean isAuthorizedActivity(IUser<Long> user, Form form, IActivity activity) {
+        if (form == null || form.getOrganizationId() == null) {
+            return false;
+        }
+        return isAuthorizedActivity(user, form.getOrganizationId(), activity);
+    }
 
-	private IGroup<Long> getOrganization(IUser<Long> user, Long organizationId) {
-		try {
-			Set<IGroup<Long>> organizations = getUserOrganizations(user);
-			for (IGroup<Long> organization : organizations) {
-				if (organization.getUniqueId().equals(organizationId)) {
-					return organization;
-				}
-			}
-		} catch (UserManagementException e) {
-			SecurityLogger.errorMessage(this.getClass().getName(), e);
-		}
+    @Override
+    public boolean isAuthorizedActivity(IUser<Long> user, Long organizationId, IActivity activity) {
+        if (organizationId == null) {
+            return false;
+        }
+        IGroup<Long> organization = getOrganization(user, organizationId);
+        if (organization == null) {
+            return false;
+        }
+        try {
+            return getActivityManager().isAuthorizedActivity(user, organization, activity);
+        } catch (UserManagementException | UserDoesNotExistException | OrganizationDoesNotExistException |
+                 InvalidCredentialsException e) {
+            SecurityLogger.errorMessage(this.getClass().getName(), e);
+            // For security
+            return false;
+        }
+    }
 
-		return null;
-	}
+    private IGroup<Long> getOrganization(IUser<Long> user, Long organizationId) {
+        try {
+            Set<IGroup<Long>> organizations = getUserOrganizations(user);
+            for (IGroup<Long> organization : organizations) {
+                if (organization.getUniqueId().equals(organizationId)) {
+                    return organization;
+                }
+            }
+        } catch (UserManagementException e) {
+            SecurityLogger.errorMessage(this.getClass().getName(), e);
+        }
 
-	@Override
-	public Set<IGroup<Long>> getUserOrganizationsWhereIsAuthorized(IUser<Long> user, IActivity activity) {
-		Set<IGroup<Long>> organizations = new HashSet<>();
-		try {
-			organizations = getUserOrganizations(user);
-			Iterator<IGroup<Long>> itr = organizations.iterator();
-			while (itr.hasNext()) {
-				IGroup<Long> organization = itr.next();
-				if (!getAuthorizationService().isAuthorizedActivity(user, organization, activity)) {
-					itr.remove();
-				}
-			}
-		} catch (UserManagementException e) {
-			SecurityLogger.errorMessage(this.getClass().getName(), e);
-		}
-		SecurityLogger.info(this.getClass().getName(),
-				"User '" + user + "' is authorized for '" + activity + "' at '" + organizations + "'.");
-		return organizations;
-	}
+        return null;
+    }
 
-	protected boolean isAuthorizedToForm(Form form, IUser<Long> user) {
-		return isAuthorizedActivity(user, form, AbcdActivity.FORM_EDITING);
-	}
+    @Override
+    public Set<IGroup<Long>> getUserOrganizationsWhereIsAuthorized(IUser<Long> user, IActivity activity) {
+        Set<IGroup<Long>> organizations = new HashSet<>();
+        try {
+            organizations = getUserOrganizations(user);
+            Iterator<IGroup<Long>> itr = organizations.iterator();
+            while (itr.hasNext()) {
+                IGroup<Long> organization = itr.next();
+                if (!getActivityManager().isAuthorizedActivity(user, organization, activity)) {
+                    itr.remove();
+                }
+            }
+        } catch (UserManagementException | UserDoesNotExistException | OrganizationDoesNotExistException |
+                 InvalidCredentialsException e) {
+            SecurityLogger.errorMessage(this.getClass().getName(), e);
+        }
+        SecurityLogger.info(this.getClass().getName(),
+                "User '" + user + "' is authorized for '" + activity + "' at '" + organizations + "'.");
+        return organizations;
+    }
 
-	@Override
-	public boolean isAuthorizedToForm(Long formOrganizationId, IUser<Long> user) {
-		return isAuthorizedActivity(user, formOrganizationId, AbcdActivity.FORM_EDITING);
-	}
+    protected boolean isAuthorizedToForm(Form form, IUser<Long> user) {
+        return isAuthorizedActivity(user, form, AbcdActivity.FORM_EDITING);
+    }
 
-	@Override
-	public Set<IGroup<Long>> getUserOrganizations(IUser<Long> user) throws UserManagementException {
-		return getAuthorizationService().getUserOrganizations(user);
-	}
+    @Override
+    public boolean isAuthorizedToForm(Long formOrganizationId, IUser<Long> user) {
+        return isAuthorizedActivity(user, formOrganizationId, AbcdActivity.FORM_EDITING);
+    }
 
-	@Override
-	public boolean isAuthorizedActivity(IUser<Long> user, IActivity activity) throws UserManagementException {
-		return getAuthorizationService().isAuthorizedActivity(user, activity);
-	}
+    @Override
+    public Set<IGroup<Long>> getUserOrganizations(IUser<Long> user) throws UserManagementException {
+        try {
+            return getAuthorizationService().getUserOrganizations(user);
+        } catch (UserDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 
-	@Override
-	public void reset() {
-		getAuthorizationService().reset();
-	}
+    @Override
+    public boolean isAuthorizedActivity(IUser<Long> user, IActivity activity) throws UserManagementException {
+        try {
+            return getActivityManager().isAuthorizedActivity(user, activity);
+        } catch (UserDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 
-	@Override
-	public IGroup<Long> getOrganization(long organizationId) throws UserManagementException {
-		return getAuthorizationService().getOrganization(organizationId);
-	}
+    @Override
+    public void reset() {
+        getAuthorizationService().reset();
+    }
 
-	@Override
-	public boolean isAuthorizedActivity(IUser<Long> user, IGroup<Long> organization, IActivity activity) throws UserManagementException {
-		return getAuthorizationService().isAuthorizedActivity(user, organization, activity);
-	}
+    @Override
+    public IGroup<Long> getOrganization(long organizationId) throws UserManagementException {
+        try {
+            return getAuthorizationService().getOrganization(organizationId);
+        } catch (OrganizationDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 
-	@Override
-	public IUser<Long> getUserByEmail(String userEmail) throws UserManagementException, UserDoesNotExistException {
-		return getAuthenticationService().getUserByEmail(userEmail);
-	}
+    @Override
+    public boolean isAuthorizedActivity(IUser<Long> user, IGroup<Long> organization, IActivity activity) throws UserManagementException {
+        try {
+            return getActivityManager().isAuthorizedActivity(user, organization, activity);
+        } catch (UserDoesNotExistException | OrganizationDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 
-	@Override
-	public IAuthorizationService<Long, Long, Long> getAuthorizationService() {
-		return authorizationService;
-	}
+    @Override
+    public IUser<Long> getUserByEmail(String userEmail) throws UserManagementException, UserDoesNotExistException {
+        try {
+            return getAuthenticationService().getUserByEmail(userEmail);
+        } catch (InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 
-	public void setAuthenticationService(IAuthenticationService<Long, Long> authenticationService) {
-		this.authenticationService = authenticationService;
-	}
+    @Override
+    public IAuthorizationService<Long, Long, Long> getAuthorizationService() {
+        return authorizationService;
+    }
 
-	@Override
-	public IAuthenticationService<Long, Long> getAuthenticationService() {
-		return authenticationService;
-	}
+    public void setAuthenticationService(IAuthenticationService<Long, Long> authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
-	public void setAuthorizationService(IAuthorizationService<Long, Long, Long> authorizationService) {
-		this.authorizationService = authorizationService;
-	}
+    @Override
+    public IAuthenticationService<Long, Long> getAuthenticationService() {
+        return authenticationService;
+    }
 
-	@Override
-	public IUser<Long> getUserById(Long userId) throws UserManagementException {
-		if (userId == null) {
-			return null;
-		}
-		return authenticationService.getUserById(userId);
-	}
+    public void setAuthorizationService(IAuthorizationService<Long, Long, Long> authorizationService) {
+        this.authorizationService = authorizationService;
+    }
+
+    public IActivityManager<Long, Long, Long> getActivityManager() {
+        return activityManager;
+    }
+
+    public void setActivityManager(IActivityManager<Long, Long, Long> activityManager) {
+        this.activityManager = activityManager;
+    }
+
+    @Override
+    public IUser<Long> getUserById(Long userId) throws UserManagementException {
+        if (userId == null) {
+            return null;
+        }
+        try {
+            return authenticationService.getUserById(userId);
+        } catch (UserDoesNotExistException | InvalidCredentialsException e) {
+            throw new UserManagementException("Error when connecting to the User Manager System!", e);
+        }
+    }
 }
