87a2b5da36616e270f3de607b2ea734d99bf3e70
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-04-13 15:36:20 +0200

    Updating RoleActivities to new interface.

--- abcd-core/src/main/java/com/biit/abcd/core/security/RoleActivities.java
+++ abcd-core/src/main/java/com/biit/abcd/core/security/RoleActivities.java
@@ -1,146 +1,153 @@
 package com.biit.abcd.core.security;
 
+import com.biit.usermanager.entity.IRole;
+import com.biit.usermanager.security.IActivity;
+import com.biit.usermanager.security.IRoleActivities;
+import com.biit.webservice.rest.RestServiceActivity;
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.stereotype.Repository;
-
-import com.biit.usermanager.entity.IRole;
-import com.biit.usermanager.security.IActivity;
-import com.biit.usermanager.security.IRoleActivities;
-import com.biit.webservice.rest.RestServiceActivity;
-
 @Repository
 public class RoleActivities implements IRoleActivities {
 
-	/**
-	 * Can create and edit forms.
-	 */
-	private static final AbcdActivity[] MANAGE_FORMS_ACTIVITIES = {
+    /**
+     * Can create and edit forms.
+     */
+    private static final AbcdActivity[] MANAGE_FORMS_ACTIVITIES = {
 
-	AbcdActivity.FORM_EDITING,
+            AbcdActivity.FORM_EDITING,
 
-	AbcdActivity.FORM_CREATE,
+            AbcdActivity.FORM_CREATE,
 
-	AbcdActivity.FORM_CHANGE_GROUP,
+            AbcdActivity.FORM_CHANGE_GROUP,
 
-	AbcdActivity.FORM_VERSION,
+            AbcdActivity.FORM_VERSION,
 
-	AbcdActivity.FORM_STATUS_UPGRADE,	
-	
-	AbcdActivity.FORM_EXPORT,
+            AbcdActivity.FORM_STATUS_UPGRADE,
 
-	AbcdActivity.PUBLISH_TO_KNOWLEDGE_MANAGER
+            AbcdActivity.FORM_EXPORT,
 
-	};
+            AbcdActivity.PUBLISH_TO_KNOWLEDGE_MANAGER
 
-	/**
-	 * Can only read forms.
-	 */
-	private static final AbcdActivity[] READ_ONLY = {
+    };
 
-	AbcdActivity.READ,
+    /**
+     * Can only read forms.
+     */
+    private static final AbcdActivity[] READ_ONLY = {
 
-	AbcdActivity.USER_EDIT_OWN_DATA
+            AbcdActivity.READ,
 
-	};
+            AbcdActivity.USER_EDIT_OWN_DATA
 
-	/**
-	 * Can do administration task for forms. Also has by default all ABCD
-	 * manager permissions.
-	 */
-	private static final AbcdActivity[] FORM_ADMINISTRATOR_EXTRA_PERMISSIONS = {
+    };
 
-	AbcdActivity.FORM_STATUS_DOWNGRADE,
+    /**
+     * Can do administration task for forms. Also has by default all ABCD
+     * manager permissions.
+     */
+    private static final AbcdActivity[] FORM_ADMINISTRATOR_EXTRA_PERMISSIONS = {
 
-	AbcdActivity.FORM_REMOVE,
+            AbcdActivity.FORM_STATUS_DOWNGRADE,
 
-	};
+            AbcdActivity.FORM_REMOVE,
 
-	private static final AbcdActivity[] MANAGE_GLOBAL_CONSTANTS = {
+    };
 
-	AbcdActivity.GLOBAL_VARIABLE_EDITOR
+    private static final AbcdActivity[] MANAGE_GLOBAL_CONSTANTS = {
 
-	};
+            AbcdActivity.GLOBAL_VARIABLE_EDITOR
 
-	/**
-	 * Manage general application options.
-	 */
-	private static final AbcdActivity[] APPLICATION_ADMINISTRATOR_EXTRA_PERMISSIONS = {
+    };
 
-	AbcdActivity.EVICT_CACHE
+    /**
+     * Manage general application options.
+     */
+    private static final AbcdActivity[] APPLICATION_ADMINISTRATOR_EXTRA_PERMISSIONS = {
 
-	};
+            AbcdActivity.EVICT_CACHE
 
-	private static final RestServiceActivity[] WEB_SERVICES_PERMISSIONS = {
+    };
 
-	RestServiceActivity.USE_WEB_SERVICE
+    private static final RestServiceActivity[] WEB_SERVICES_PERMISSIONS = {
 
-	};
+            RestServiceActivity.USE_WEB_SERVICE
 
-	private static List<IActivity> formManagerPermissions = new ArrayList<IActivity>();
-	private static List<IActivity> readOnlyPermissions = new ArrayList<IActivity>();
-	private static List<IActivity> formAdministratorPermissions = new ArrayList<IActivity>();
-	private static List<IActivity> globalConstantsAdministratorPermissions = new ArrayList<IActivity>();
-	private static List<IActivity> applicationAdministratorPermissions = new ArrayList<IActivity>();
-	private static List<IActivity> webServiceUserPermissions = new ArrayList<IActivity>();
+    };
 
-	static {
-		for (AbcdActivity activity : FORM_ADMINISTRATOR_EXTRA_PERMISSIONS) {
-			formAdministratorPermissions.add(activity);
-		}
-		for (AbcdActivity activity : MANAGE_FORMS_ACTIVITIES) {
-			formManagerPermissions.add(activity);
-		}
-		for (AbcdActivity activity : READ_ONLY) {
-			readOnlyPermissions.add(activity);
-		}
-		for (AbcdActivity activity : MANAGE_GLOBAL_CONSTANTS) {
-			globalConstantsAdministratorPermissions.add(activity);
-		}
-		for (AbcdActivity activity : APPLICATION_ADMINISTRATOR_EXTRA_PERMISSIONS) {
-			applicationAdministratorPermissions.add(activity);
-		}
-		for (RestServiceActivity activity : WEB_SERVICES_PERMISSIONS) {
-			webServiceUserPermissions.add(activity);
-		}
+    private static List<IActivity> formManagerPermissions = new ArrayList<IActivity>();
+    private static List<IActivity> readOnlyPermissions = new ArrayList<IActivity>();
+    private static List<IActivity> formAdministratorPermissions = new ArrayList<IActivity>();
+    private static List<IActivity> globalConstantsAdministratorPermissions = new ArrayList<IActivity>();
+    private static List<IActivity> applicationAdministratorPermissions = new ArrayList<IActivity>();
+    private static List<IActivity> webServiceUserPermissions = new ArrayList<IActivity>();
 
-	}
+    static {
+        for (AbcdActivity activity : FORM_ADMINISTRATOR_EXTRA_PERMISSIONS) {
+            formAdministratorPermissions.add(activity);
+        }
+        for (AbcdActivity activity : MANAGE_FORMS_ACTIVITIES) {
+            formManagerPermissions.add(activity);
+        }
+        for (AbcdActivity activity : READ_ONLY) {
+            readOnlyPermissions.add(activity);
+        }
+        for (AbcdActivity activity : MANAGE_GLOBAL_CONSTANTS) {
+            globalConstantsAdministratorPermissions.add(activity);
+        }
+        for (AbcdActivity activity : APPLICATION_ADMINISTRATOR_EXTRA_PERMISSIONS) {
+            applicationAdministratorPermissions.add(activity);
+        }
+        for (RestServiceActivity activity : WEB_SERVICES_PERMISSIONS) {
+            webServiceUserPermissions.add(activity);
+        }
 
-	@Override
-	public Set<IActivity> getRoleActivities(IRole<Long> role) {
-		Set<IActivity> activities = new HashSet<IActivity>();
-		AbcdRoles abcdRole = AbcdRoles.parseTag(role.getUniqueName());
-		switch (abcdRole) {
-		case FORM_ADMIN:
-			activities.addAll(formAdministratorPermissions);
-			activities.addAll(formManagerPermissions);
-			activities.addAll(readOnlyPermissions);
-			break;
-		case FORM_EDIT:
-			activities.addAll(formManagerPermissions);
-			activities.addAll(readOnlyPermissions);
-			break;
-		case READ:
-			activities.addAll(readOnlyPermissions);
-			break;
-		case NULL:
-			break;
-		case GLOBAL_CONSTANTS:
-			activities.addAll(globalConstantsAdministratorPermissions);
-			activities.addAll(readOnlyPermissions);
-			break;
-		case APPLICATION_ADMIN:
-			activities.addAll(applicationAdministratorPermissions);
-			activities.addAll(readOnlyPermissions);
-			break;
-		case WEB_SERVICE_USER:
-			activities.addAll(webServiceUserPermissions);
-			break;
-		}
-		return activities;
-	}
+    }
+
+    @Override
+    public Set<IActivity> getRoleActivities(IRole<Long> role) {
+        if (role != null) {
+            return getRoleActivities(role.getUniqueName());
+        }
+        return new HashSet<>();
+    }
+
+    @Override
+    public Set<IActivity> getRoleActivities(String roleName) {
+        Set<IActivity> activities = new HashSet<>();
+        AbcdRoles abcdRole = AbcdRoles.parseTag(roleName);
+        switch (abcdRole) {
+            case FORM_ADMIN:
+                activities.addAll(formAdministratorPermissions);
+                activities.addAll(formManagerPermissions);
+                activities.addAll(readOnlyPermissions);
+                break;
+            case FORM_EDIT:
+                activities.addAll(formManagerPermissions);
+                activities.addAll(readOnlyPermissions);
+                break;
+            case READ:
+                activities.addAll(readOnlyPermissions);
+                break;
+            case NULL:
+                break;
+            case GLOBAL_CONSTANTS:
+                activities.addAll(globalConstantsAdministratorPermissions);
+                activities.addAll(readOnlyPermissions);
+                break;
+            case APPLICATION_ADMIN:
+                activities.addAll(applicationAdministratorPermissions);
+                activities.addAll(readOnlyPermissions);
+                break;
+            case WEB_SERVICE_USER:
+                activities.addAll(webServiceUserPermissions);
+                break;
+        }
+        return activities;
+    }
 
 }
