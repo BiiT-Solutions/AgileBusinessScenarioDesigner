92fe4a9ab4925a1fb875fbe97097b91357fa48ed
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-04-17 09:26:42 +0200

    Adding Ctrl + click for answer selection

--- abcd-gui/src/main/java/com/biit/abcd/webpages/components/StringInputWindow.java
+++ abcd-gui/src/main/java/com/biit/abcd/webpages/components/StringInputWindow.java
@@ -1,8 +1,5 @@
 package com.biit.abcd.webpages.components;
 
-import java.text.ParseException;
-import java.util.Date;
-
 import com.biit.abcd.configuration.AbcdConfigurationReader;
 import com.biit.abcd.language.AnswerFormatUi;
 import com.biit.abcd.language.LanguageCodes;
@@ -11,7 +8,6 @@ import com.biit.abcd.language.UserLocaleStringToDoubleConverter;
 import com.biit.abcd.persistence.entity.AnswerFormat;
 import com.biit.abcd.persistence.entity.expressions.ExpressionValueTimestamp;
 import com.vaadin.data.Property.ReadOnlyException;
-import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.util.ObjectProperty;
@@ -26,171 +22,172 @@ import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 
+import java.text.ParseException;
+import java.util.Date;
+
 public class StringInputWindow extends AcceptCancelWindow {
-	private static final long serialVersionUID = 361486551550136464L;
-	private static final String WIDTH = "400px";
-	private static final String HEIGHT = "250px";
-	private static final String FIELD_WIDTH = "75%";
+    private static final long serialVersionUID = 361486551550136464L;
+    private static final String WIDTH = "400px";
+    private static final String HEIGHT = "250px";
+    private static final String FIELD_WIDTH = "75%";
 
-	private AbstractField<?> expressionValue;
-	private ComboBox expressionType;
+    private AbstractField<?> expressionValue;
+    private ComboBox expressionType;
 
-	private FormLayout formLayout;
+    private FormLayout formLayout;
 
-	public StringInputWindow() {
-		super();
-		setContent(generateContent());
-		setResizable(true);
-		setDraggable(false);
-		setClosable(false);
-		setModal(true);
-		setWidth(WIDTH);
-		setHeight(HEIGHT);
-	}
+    public StringInputWindow() {
+        super();
+        setContent(generateContent());
+        setResizable(true);
+        setDraggable(false);
+        setClosable(false);
+        setModal(true);
+        setWidth(WIDTH);
+        setHeight(HEIGHT);
+    }
 
-	public String getValue() {
-		if (expressionValue != null) {
-			try {
-				expressionValue.validate();
-				if (getFormat().equals(AnswerFormat.NUMBER) && expressionValue.getConvertedValue() != null) {
-					return expressionValue.getConvertedValue().toString();
-				} else if (getFormat().equals(AnswerFormat.DATE)) {
-					return ExpressionValueTimestamp.getFormatter().format((Date) expressionValue.getValue());
-				}
-				return expressionValue.getValue().toString();
-			} catch (InvalidValueException e) {
-				// Error shown to the user in the caller class
-			}
-		}
-		return null;
-	}
+    public String getValue() {
+        if (expressionValue != null) {
+            try {
+                expressionValue.validate();
+                if (getFormat().equals(AnswerFormat.NUMBER) && expressionValue.getConvertedValue() != null) {
+                    return expressionValue.getConvertedValue().toString();
+                } else if (getFormat().equals(AnswerFormat.DATE)) {
+                    return ExpressionValueTimestamp.getFormatter().format((Date) expressionValue.getValue());
+                }
+                return expressionValue.getValue().toString();
+            } catch (InvalidValueException e) {
+                // Error shown to the user in the caller class
+            }
+        }
+        return null;
+    }
 
-	public AnswerFormat getFormat() {
-		return (AnswerFormat) expressionType.getValue();
-	}
+    public AnswerFormat getFormat() {
+        return (AnswerFormat) expressionType.getValue();
+    }
 
-	private Component generateContent() {
-		HorizontalLayout rootLayout = new HorizontalLayout();
-		rootLayout.setSizeFull();
-		rootLayout.setMargin(true);
-		rootLayout.setImmediate(true);
+    private Component generateContent() {
+        HorizontalLayout rootLayout = new HorizontalLayout();
+        rootLayout.setSizeFull();
+        rootLayout.setMargin(true);
+        rootLayout.setImmediate(true);
 
-		formLayout = new FormLayout();
-		formLayout.setSizeUndefined();
-		formLayout.setSpacing(true);
-		formLayout.setImmediate(true);
+        formLayout = new FormLayout();
+        formLayout.setSizeUndefined();
+        formLayout.setSpacing(true);
+        formLayout.setImmediate(true);
 
-		expressionType = new ComboBox(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TYPE));
-		expressionType.setImmediate(true);
-		for (AnswerFormatUi answerFormatUi : AnswerFormatUi.values()) {
-			expressionType.addItem(answerFormatUi.getAnswerFormat());
-			expressionType.setItemCaption(answerFormatUi.getAnswerFormat(), ServerTranslate.translate(answerFormatUi.getLanguageCode()));
-		}
-		expressionType.setValue(AnswerFormatUi.values()[0].getAnswerFormat());
-		expressionType.addValueChangeListener((ValueChangeListener) event -> {
+        expressionType = new ComboBox(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TYPE));
+        expressionType.setImmediate(true);
+        for (AnswerFormatUi answerFormatUi : AnswerFormatUi.values()) {
+            expressionType.addItem(answerFormatUi.getAnswerFormat());
+            expressionType.setItemCaption(answerFormatUi.getAnswerFormat(), ServerTranslate.translate(answerFormatUi.getLanguageCode()));
+        }
+        expressionType.setValue(AnswerFormatUi.values()[0].getAnswerFormat());
+        expressionType.addValueChangeListener((ValueChangeListener) event -> {
             createTextField();
             setLocale();
             setPromt();
         });
 
-		expressionType.setNullSelectionAllowed(false);
-		expressionType.setWidth(FIELD_WIDTH);
+        expressionType.setNullSelectionAllowed(false);
+        expressionType.setWidth(FIELD_WIDTH);
 
-		formLayout.addComponent(expressionType);
+        formLayout.addComponent(expressionType);
 
-		createTextField();
+        createTextField();
 
-		rootLayout.addComponent(formLayout);
-		rootLayout.setComponentAlignment(formLayout, Alignment.MIDDLE_CENTER);
+        rootLayout.addComponent(formLayout);
+        rootLayout.setComponentAlignment(formLayout, Alignment.MIDDLE_CENTER);
 
-		setPromt();
-		return rootLayout;
-	}
+        setPromt();
+        return rootLayout;
+    }
 
-	private void createTextField() {
-		if (expressionValue != null) {
-			formLayout.removeComponent(expressionValue);
-		}
+    private void createTextField() {
+        if (expressionValue != null) {
+            formLayout.removeComponent(expressionValue);
+        }
 
-		if (expressionType.getValue().equals(AnswerFormat.DATE)) {
-			expressionValue = new DateField(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TEXTFIELD));
-		} else {
-			expressionValue = new TextField(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TEXTFIELD));
-		}
-		expressionValue.setImmediate(true);
-		expressionValue.setWidth(FIELD_WIDTH);
-		if (expressionValue instanceof TextField) {
-			((TextField) expressionValue).setNullRepresentation("");
-		}
-		expressionValue.focus();
+        if (expressionType.getValue().equals(AnswerFormat.DATE)) {
+            expressionValue = new DateField(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TEXTFIELD));
+        } else {
+            expressionValue = new TextField(ServerTranslate.translate(LanguageCodes.EXPRESSION_INPUT_WINDOW_TEXTFIELD));
+        }
+        expressionValue.setImmediate(true);
+        expressionValue.setWidth(FIELD_WIDTH);
+        if (expressionValue instanceof TextField) {
+            ((TextField) expressionValue).setNullRepresentation("");
+        }
+        expressionValue.focus();
 
-		formLayout.addComponent(expressionValue);
-	}
+        formLayout.addComponent(expressionValue);
+    }
 
-	public void setValue(String value) {
-		if (value != null) {
-			if (getFormat().equals(AnswerFormat.NUMBER)) {
-				try {
-					ObjectProperty<Double> property = new ObjectProperty<Double>(Double.parseDouble(value));
-					expressionValue.setPropertyDataSource(property);
-				} catch (NumberFormatException nfe) {
-					setValue(null);
-				}
-			} else if (getFormat().equals(AnswerFormat.TEXT)) {
-				((TextField) expressionValue).setValue(value.toString());
-			} else if (getFormat().equals(AnswerFormat.DATE)) {
-				try {
-					((DateField) expressionValue).setValue(ExpressionValueTimestamp.getFormatter().parse(value));
-				} catch (ReadOnlyException | ConversionException | ParseException e) {
-					setValue(null);
-				}
-			} else {
-				setValue(null);
-			}
-		}
-	}
+    public void setValue(String value) {
+        if (value != null) {
+            if (getFormat().equals(AnswerFormat.NUMBER)) {
+                try {
+                    ObjectProperty<Double> property = new ObjectProperty<Double>(Double.parseDouble(value));
+                    expressionValue.setPropertyDataSource(property);
+                } catch (NumberFormatException nfe) {
+                    setValue(null);
+                }
+            } else if (getFormat().equals(AnswerFormat.TEXT)) {
+                ((TextField) expressionValue).setValue(value);
+            } else if (getFormat().equals(AnswerFormat.DATE)) {
+                try {
+                    ((DateField) expressionValue).setValue(ExpressionValueTimestamp.getFormatter().parse(value));
+                } catch (ReadOnlyException | ConversionException | ParseException e) {
+                    setValue(null);
+                }
+            } else {
+                setValue(null);
+            }
+        }
+    }
 
-	public void setFormat(AnswerFormat format) {
-		expressionType.setValue(format);
-	}
+    public void setFormat(AnswerFormat format) {
+        expressionType.setValue(format);
+    }
 
-	private void setPromt() {
-		switch (getFormat()) {
-		case DATE:
-			break;
-		case NUMBER:
-			((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_FLOAT));
-			break;
-		case POSTAL_CODE:
-			((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_POSTAL_CODE));
-			expressionValue.addValidator(new RegexpValidator(AbcdConfigurationReader.getInstance().getPostalCodeMask(), ServerTranslate
-					.translate(LanguageCodes.ERROR_DATA_FORMAT_INVALID)));
-			break;
-		case MULTI_TEXT:
-		case TEXT:
-			((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_TEXT));
-			break;
-		}
-	}
+    private void setPromt() {
+        switch (getFormat()) {
+            case DATE:
+                break;
+            case NUMBER:
+                ((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_FLOAT));
+                break;
+            case POSTAL_CODE:
+                ((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_POSTAL_CODE));
+                expressionValue.addValidator(new RegexpValidator(AbcdConfigurationReader.getInstance().getPostalCodeMask(), ServerTranslate
+                        .translate(LanguageCodes.ERROR_DATA_FORMAT_INVALID)));
+                break;
+            case MULTI_TEXT:
+            case TEXT:
+                ((TextField) expressionValue).setInputPrompt(ServerTranslate.translate(LanguageCodes.INPUT_PROMPT_TEXT));
+                break;
+        }
+    }
 
-	private void setLocale() {
-		switch (getFormat()) {
-		case DATE:
-			break;
-		case NUMBER:
-			((TextField) expressionValue).setConverter(new UserLocaleStringToDoubleConverter());
-			break;
-		case POSTAL_CODE:
-			((TextField) expressionValue).setConverter(String.class);
-			break;
-		case MULTI_TEXT:
-		case TEXT:
-			((TextField) expressionValue).setConverter(String.class);
-			break;
-		}
-	}
+    private void setLocale() {
+        switch (getFormat()) {
+            case DATE:
+                break;
+            case NUMBER:
+                ((TextField) expressionValue).setConverter(new UserLocaleStringToDoubleConverter());
+                break;
+            case POSTAL_CODE:
+            case MULTI_TEXT:
+            case TEXT:
+                expressionValue.setConverter(String.class);
+                break;
+        }
+    }
 
-	public void enableExpressionType(boolean enabled) {
-		expressionType.setEnabled(enabled);
-	}
+    public void enableExpressionType(boolean enabled) {
+        expressionType.setEnabled(enabled);
+    }
 }
--- abcd-gui/src/main/java/com/biit/abcd/webpages/elements/decisiontable/AddNewAnswerExpressionWindow.java
+++ abcd-gui/src/main/java/com/biit/abcd/webpages/elements/decisiontable/AddNewAnswerExpressionWindow.java
@@ -7,140 +7,136 @@ import com.biit.abcd.persistence.entity.Question;
 import com.biit.abcd.persistence.entity.expressions.ExpressionChain;
 import com.biit.abcd.persistence.entity.expressions.ExpressionValueCustomVariable;
 import com.biit.abcd.persistence.entity.expressions.ExpressionValueTreeObjectReference;
-import com.biit.abcd.persistence.entity.rules.TableRuleRow;
 import com.biit.abcd.webpages.components.AcceptCancelClearWindow;
 import com.biit.abcd.webpages.components.SelectFormAnswerTable;
 import com.biit.abcd.webpages.elements.expression.viewer.ExpressionEditorComponent;
 import com.biit.abcd.webpages.elements.expression.viewer.SimpleExpressionEditorComponent;
 import com.biit.form.entity.TreeObject;
-import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.VerticalLayout;
 
 public class AddNewAnswerExpressionWindow extends AcceptCancelClearWindow {
-	private static final long serialVersionUID = 7699690992550597244L;
-	private SelectFormAnswerTable answerTable = null;
-	private ExpressionEditorComponent expressionEditorComponent;
-	protected ExpressionChain expressionChain;
+    private static final long serialVersionUID = 7699690992550597244L;
+    private SelectFormAnswerTable answerTable = null;
+    private ExpressionEditorComponent expressionEditorComponent;
+    protected ExpressionChain expressionChain;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param reference
-	 *            Question or element to apply the expression.
-	 * @param expressionChain
-	 *            Expression applied
-	 */
-	public AddNewAnswerExpressionWindow(ExpressionValueTreeObjectReference reference, ExpressionChain expressionChain) {
-		super();
-		this.expressionChain = (ExpressionChain) expressionChain.generateCopy();
-		setFirstElementNotEditable(this.expressionChain);
-		if (reference instanceof ExpressionValueCustomVariable) {
-			// Custom variable
-			setContent(generateExpression());
-		} else {
-			TreeObject treeObject = reference.getReference();
-			if (treeObject != null) {
-				if ((treeObject instanceof Question) && (((Question) treeObject).getAnswerType() != AnswerType.INPUT)) {
-					Question question = (Question) treeObject;
-					setContent(generateTreeObjectTable(question));
-				} else {
-					setContent(generateExpression());
-				}
-			}
-		}
-		setResizable(false);
-		setCaption(ServerTranslate.translate(LanguageCodes.CONDITION_TABLE_EDIT_CONDITION_CAPTION));
-	}
+    /**
+     * Constructor.
+     *
+     * @param reference       Question or element to apply the expression.
+     * @param expressionChain Expression applied
+     */
+    public AddNewAnswerExpressionWindow(ExpressionValueTreeObjectReference reference, ExpressionChain expressionChain) {
+        super();
+        this.expressionChain = (ExpressionChain) expressionChain.generateCopy();
+        setFirstElementNotEditable(this.expressionChain);
+        if (reference instanceof ExpressionValueCustomVariable) {
+            // Custom variable
+            setContent(generateExpression());
+        } else {
+            TreeObject treeObject = reference.getReference();
+            if (treeObject != null) {
+                if ((treeObject instanceof Question) && (((Question) treeObject).getAnswerType() != AnswerType.INPUT)) {
+                    Question question = (Question) treeObject;
+                    setContent(generateTreeObjectTable(question));
+                } else {
+                    setContent(generateExpression());
+                }
+            }
+        }
+        setResizable(false);
+        setCaption(ServerTranslate.translate(LanguageCodes.CONDITION_TABLE_EDIT_CONDITION_CAPTION));
+    }
 
-	public Component generateTreeObjectTable(Question question) {
-		setWidth("50%");
-		setHeight("75%");
-		answerTable = new SelectFormAnswerTable();
-		answerTable.setRootElement(question);
-		answerTable.setNullSelectionAllowed(true);
-		answerTable.setSelectable(true);
-		answerTable.addValueChangeListener((ValueChangeListener) event -> setSelectedTableElement());
-		answerTable.setSizeFull();
-		if (!expressionChain.getExpressions().isEmpty()
-				&& (expressionChain.getExpressions().get(expressionChain.getExpressions().size() - 1) instanceof ExpressionValueTreeObjectReference)) {
-			answerTable.setValue(((ExpressionValueTreeObjectReference) expressionChain.getExpressions().get(
-					expressionChain.getExpressions().size() - 1)).getReference());
-		} else {
-			answerTable.setValue(null);
-		}
-		setSelectedTableElement();
-		answerTable.addItemClickListener((ItemClickEvent.ItemClickListener) event -> {
-			if (event.isDoubleClick()) {
-				this.fireAcceptActionListeners();
-			}
-		});
-		return answerTable;
-	}
+    public Component generateTreeObjectTable(Question question) {
+        setWidth("50%");
+        setHeight("75%");
+        answerTable = new SelectFormAnswerTable();
+        answerTable.setRootElement(question);
+        answerTable.setNullSelectionAllowed(true);
+        answerTable.setSelectable(true);
+        answerTable.addValueChangeListener((ValueChangeListener) event -> setSelectedTableElement());
+        answerTable.setSizeFull();
+        if (!expressionChain.getExpressions().isEmpty()
+                && (expressionChain.getExpressions().get(expressionChain.getExpressions().size() - 1) instanceof ExpressionValueTreeObjectReference)) {
+            answerTable.setValue(((ExpressionValueTreeObjectReference) expressionChain.getExpressions().get(
+                    expressionChain.getExpressions().size() - 1)).getReference());
+        } else {
+            answerTable.setValue(null);
+        }
+        setSelectedTableElement();
+        answerTable.addItemClickListener((ItemClickEvent.ItemClickListener) event -> {
+            if ((event.isDoubleClick() || event.isCtrlKey()) && answerTable.getValue() != null) {
+                this.fireAcceptActionListeners();
+            }
+        });
+        return answerTable;
+    }
 
-	private void setSelectedTableElement() {
-		expressionChain.removeAllExpressions();
-		if (answerTable.getValue() != null) {
-			expressionChain.addExpression(new ExpressionValueTreeObjectReference(answerTable.getValue()));
-		}
-	}
+    private void setSelectedTableElement() {
+        expressionChain.removeAllExpressions();
+        if (answerTable.getValue() != null) {
+            expressionChain.addExpression(new ExpressionValueTreeObjectReference(answerTable.getValue()));
+        }
+    }
 
-	public Component generateExpression() {
-		setWidth("90%");
-		setHeight("90%");
+    public Component generateExpression() {
+        setWidth("90%");
+        setHeight("90%");
 
-		VerticalLayout layout = new VerticalLayout();
+        VerticalLayout layout = new VerticalLayout();
 
-		// Create content
-		expressionEditorComponent = new SimpleExpressionEditorComponent();
-		expressionEditorComponent.setSizeFull();
+        // Create content
+        expressionEditorComponent = new SimpleExpressionEditorComponent();
+        expressionEditorComponent.setSizeFull();
 
-		((SimpleExpressionEditorComponent) expressionEditorComponent).refreshExpressionEditor(expressionChain);
+        ((SimpleExpressionEditorComponent) expressionEditorComponent).refreshExpressionEditor(expressionChain);
 
-		layout.addComponent(expressionEditorComponent);
-		layout.setSizeFull();
-		layout.setMargin(true);
-		return layout;
-	}
+        layout.addComponent(expressionEditorComponent);
+        layout.setSizeFull();
+        layout.setMargin(true);
+        return layout;
+    }
 
-	public ExpressionChain getExpressionChain() {
-		if (answerTable == null) {
-			return getExpressionWithoutFirstElement();
-		}
-		return expressionChain;
-	}
+    public ExpressionChain getExpressionChain() {
+        if (answerTable == null) {
+            return getExpressionWithoutFirstElement();
+        }
+        return expressionChain;
+    }
 
-	private ExpressionChain getExpressionWithoutFirstElement() {
-		ExpressionChain expressionChain = (ExpressionChain) this.expressionChain.generateCopy();
-		if ((expressionChain != null) && !expressionChain.getExpressions().isEmpty()) {
-			expressionChain.removeFirstExpression();
-		}
-		return expressionChain;
-	}
-	
-	private void setFirstElementNotEditable(ExpressionChain expressionChain){
-		if ((expressionChain != null) && !expressionChain.getExpressions().isEmpty()) {
-			expressionChain.getExpressions().get(0).setEditable(false);
-		}
-	}
+    private ExpressionChain getExpressionWithoutFirstElement() {
+        ExpressionChain expressionChain = (ExpressionChain) this.expressionChain.generateCopy();
+        if ((expressionChain != null) && !expressionChain.getExpressions().isEmpty()) {
+            expressionChain.removeFirstExpression();
+        }
+        return expressionChain;
+    }
 
-	public void clearSelection() {
-		if (answerTable != null) {
-			answerTable.setValue(null);
-		}
-		if (expressionEditorComponent != null) {
-			((SimpleExpressionEditorComponent) expressionEditorComponent).clear();
-		}
-		expressionChain = new ExpressionChain();
-	}
+    private void setFirstElementNotEditable(ExpressionChain expressionChain) {
+        if ((expressionChain != null) && !expressionChain.getExpressions().isEmpty()) {
+            expressionChain.getExpressions().get(0).setEditable(false);
+        }
+    }
 
-	protected ExpressionEditorComponent getExpressionEditorComponent() {
-		return expressionEditorComponent;
-	}
+    public void clearSelection() {
+        if (answerTable != null) {
+            answerTable.setValue(null);
+        }
+        if (expressionEditorComponent != null) {
+            ((SimpleExpressionEditorComponent) expressionEditorComponent).clear();
+        }
+        expressionChain = new ExpressionChain();
+    }
 
-	protected void setExpressionEditorComponent(ExpressionEditorComponent expressionEditorComponent) {
-		this.expressionEditorComponent = expressionEditorComponent;
-	}
+    protected ExpressionEditorComponent getExpressionEditorComponent() {
+        return expressionEditorComponent;
+    }
+
+    protected void setExpressionEditorComponent(ExpressionEditorComponent expressionEditorComponent) {
+        this.expressionEditorComponent = expressionEditorComponent;
+    }
 }
